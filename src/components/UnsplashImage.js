adaca583557dc0349c3064380194a320c8b6cf5c
Ishan Desai <idd24@drexel.edu>
2024-01-08 16:24:06 -0500

    base image section complete

--- src/components/UnsplashImage.js
+++ src/components/UnsplashImage.js
@@ -1,41 +1,55 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react';
 
 export const UnsplashImage = () => {
 	const [activeImageUrl, setActiveImageUrl] = useState('')
 	const [activeImageObject, setActiveImageObject] = useState({})
+	const [activeImageLocation, setActiveImageLocation] = useState('')
 	const [imagesList, setImageList] = useState([])
 	
 	const fetchRandomImage = (list) => {
-		let num = Math.floor(Math.random() * 30);
+		let num = Math.floor(Math.random() * list.length);
 		let randomImage = list[num]
 		setActiveImageObject(randomImage);
-		console.log(randomImage)
-		console.log("activeImageUrl:", activeImageUrl)
-		
 		if (randomImage != null) {
 			let url = randomImage.urls.regular
+			let location = randomImage.location.country
 			setActiveImageUrl(url)
-			console.log(url)
+			setActiveImageLocation(location)
 		}
-	} 
+	};
 
 	const fetchImagesList = async () => {
 		try {
-			const response = await fetch(
-				'https://api.unsplash.com/photos/?query=nature&client_id=pmTNZxpE0TQ6pRwUqnkTL8uWNhzcEWYQzPdzd8NtmPo&per_page=30&order_by=popular')
-			const data = await response.json()
+			const response = await fetch('https://api.unsplash.com//photos/random?query=nature&count=30', {
+				headers: {
+					'Authorization': 'Client-ID pmTNZxpE0TQ6pRwUqnkTL8uWNhzcEWYQzPdzd8NtmPo'
+				}
+			});
+			const data = await response.json();
+			let filtered_list = []
 
-			setImageList(data)
+			for (let i=0; i<data.length; i++) {
+				let obj = data[i]
+				if (obj.location.country != null) {
+					filtered_list.push(obj)
+				}
+			}
+			setImageList(filtered_list)
 		}
 		catch {
 			console.error("Error fetching images from API, Please try again!")
 		}
-	}
+	};
+
+	useEffect(() => {
+		if (imagesList.length > 0) {
+		  fetchRandomImage(imagesList);
+		}
+	}, [imagesList]);
 
 	const handlePlayClick = () => {
 		fetchImagesList();
-		fetchRandomImage(imagesList)
-	}
+	};
 
 	return (
 		<div className='ImageSection'>
@@ -43,5 +57,5 @@ export const UnsplashImage = () => {
 			<button onClick={handlePlayClick}>Play</button>
 			<img src={activeImageUrl} alt='' className='randomImage' />
 		</div>
-	)
+	);
 }
